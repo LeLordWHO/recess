591fa90f5c1c8ee95a4e7e41e7c6ce0abe5650ec
Leonard Hardiman <leonardebay@gmail.com>
2023-09-17 15:18:55 +0200

    improved scroll fix

--- accordion_scroll_fix.js
+++ accordion_scroll_fix.js
@@ -3,15 +3,18 @@ $('.accordion').on('shown.bs.collapse', function (event) {
     const rect = accordionHeader[0].getBoundingClientRect();
   
     // Offset value to scroll above the accordion header
-    const offset = 100; // You can tweak this value
+    const offset = 80; // You can tweak this value
   
-    // Check if the top of the accordion header is not visible
-    if (rect.top < offset) {
-      const scrollPosition = window.scrollY + rect.top - offset;
-      window.scrollTo({
-        top: scrollPosition,
-        behavior: 'smooth'
-      });
+    // Check if the accordion item is expanding
+    if ($(event.target).hasClass('show')) {
+      // Check if the top of the accordion header is not visible
+      if (rect.top < -80) {
+        const scrollPosition = window.scrollY + rect.top - offset;
+        window.scrollTo({
+          top: scrollPosition,
+          behavior: 'smooth'
+        });
+      }
     }
   });
   
